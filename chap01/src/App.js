150007c74ea15ffe26dfa6db0ac57ad7a5f0e658
nogada <azxca1731@gmail.com>
2018-07-19 00:17:25 +0900

    Props 기본 개념 이해

--- chap01/src/App.js
+++ chap01/src/App.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyName from './MyName';
 
 class App extends Component {
   render() {
-    const style = {
-      backgroundColor: 'black',
-      padding: '16px',
-      color: 'white',
-      fontSize: '12px'
-    };
-    const value = 1;
     return (
-      <div>
-        <div style={style}>
-          hi there
-        </div>
-        <div>
-          {
-            (() => {
-              if (value === 1) return (<div>하나</div>);
-              if (value === 2) return (<div>둘</div>);
-              if (value === 3) return (<div>셋</div>);
-            })()
-          }
-        </div>
-      </div>
+      <MyName name="리액트" />
     );
   }
 }
