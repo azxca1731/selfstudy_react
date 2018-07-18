5b547c3ff975d668335d6fcbbdf1410f829e6e95
nogada <azxca1731@gmail.com>
2018-07-19 03:47:57 +0900

    컴포넌트 분할 및 에러수정

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import PhoneForm from './PhoneForm';
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList'
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
   render() {
     return (
       <div className="App">
-        <PhoneForm />
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+        <PhoneInfoList data={this.state.information} />
       </div>
     );
   }
--- phone-book/src/components/PhoneForm.js
+++ phone-book/src/components/PhoneForm.js
@@ -7,13 +7,23 @@ class PhoneForm extends Component {
     }
     handleChange = (e) => {
         this.setState({
-            //다중 이벤트 처리에는 이렇게
             [e.target.name]: e.target.value
-        });
+        })
+    }
+    handleSubmit = (e) => {
+        // 페이지 리로딩 방지
+        e.preventDefault();
+        // 상태값을 onCreate 를 통하여 부모에게 전달
+        this.props.onCreate(this.state);
+        // 상태 초기화
+        this.setState({
+            name: '',
+            phone: ''
+        })
     }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input
                     placeholder="이름"
                     value={this.state.name}
@@ -26,9 +36,10 @@ class PhoneForm extends Component {
                     onChange={this.handleChange}
                     name="phone"
                 />
-                <div>{this.state.name} {this.state.phone}</div>
+                <button type="submit">등록</button>
             </form>
-        )
+        );
     }
 }
+
 export default PhoneForm;
