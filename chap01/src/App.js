59a459b641f616ba98639ec47e7438d1d788ad68
nogada <azxca1731@gmail.com>
2018-07-18 23:50:58 +0900

    jsx 안에서 자바 스크립트 값 사용 예시

--- chap01/src/App.js
+++ chap01/src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'react';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        hello {name}!
       </div>
     );
   }
