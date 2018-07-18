642763f0e22fb52d016130f1806a5a4b1c9d2673
nogada <azxca1731@gmail.com>
2018-07-19 03:11:36 +0900

    기본입력 추가

---
+++ phone-book/src/PhoneForm.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+    state = {
+        name: ''
+    }
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    render() {
+        return (
+            <form>
+                <input
+                    placeholder="이름"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+                <div>{this.state.name}</div>
+            </form>
+        )
+    }
+}
+export default PhoneForm;
