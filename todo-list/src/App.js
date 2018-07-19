dfe5cabc2ac6e8d8bcb070d6718ee7385b267b3f
nogada <azxca1731@gmail.com>
2018-07-19 21:07:19 +0900

    ToDd template success

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoListTemplate from './components/TodoListTemplate'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <TodoListTemplate form={<div>이렇게 말이죠.</div>}>
+        <div>여기엔 children 자리구요.</div>
+      </TodoListTemplate>
     );
   }
 }
