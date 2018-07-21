b7cbceeea9a0cc26a37aff15ae5c8634384b9e32
nogada <azxca1731@gmail.com>
2018-07-21 12:58:26 +0900

    index refactoring and add Youtube search

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
 import API_KEY from './api';
 
 
-//새 컴포넌트 생성 컴포넌트는 html생성
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
 
+//새 컴포넌트 생성 컴포넌트는 html생성
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        }
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos })
+            //= this.setState({ videos: videos})
+        })
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 }
 //컴포넌트 삽입 및 페이지 반영 
 ReactDom.render(<App />, document.querySelector('.container'));
