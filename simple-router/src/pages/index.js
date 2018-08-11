d8a627426fcf8327d2bbdc5eba991ec0d0d77ab0
nogada <azxca1731@gmail.com>
2018-08-11 16:23:35 +0900

    git split

--- simple-router/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Posts } from './Posts';
-export { default as Whoops404 } from './Whoops404';
