a9eaa8d395031f4025298622688cdb40a845cb3f
azxca1731 <azxca1731@gmail.com>
2019-02-04 18:11:04 +0900

    디버그 모드

--- jest-enzyme/src/App.test.js
+++ jest-enzyme/src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+it("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
 });
