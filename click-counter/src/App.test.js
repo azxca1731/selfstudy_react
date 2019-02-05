f4a1f3c03e04935a6a7773e0b424f0e2ad4c10f9
azxca1731 <azxca1731@gmail.com>
2019-02-05 15:55:19 +0900

    DRY refactor

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -6,21 +6,42 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create shallow wrapper <App />
+ * @function setup
+ * @param {object} props
+ * @param {any} state
+ * @returns {ShalloWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />);
+};
+
+/**
+ * Factory function to search attribute for wrapper
+ * @function findByTestAttr
+ * @param {*} wrapper
+ * @param {*} val
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
 test("render without error", () => {
-  const wrapper = shallow(<App />);
-  const Appcontent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const Appcontent = findByTestAttr(wrapper, "component-app");
   expect(Appcontent.length).toBe(1);
 });
 
 test("render increase button", () => {
-  const wrapper = shallow(<App />);
-  const increaseButton = wrapper.find("[data-test='increase-button']");
+  const wrapper = setup();
+  const increaseButton = findByTestAttr(wrapper, "increase-button");
   expect(increaseButton.length).toBe(1);
 });
 
 test("render counter display", () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
 
