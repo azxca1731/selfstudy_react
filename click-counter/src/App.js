985f4a50e47513204ab76135a97ca197d31b50a7
azxca1731 <azxca1731@gmail.com>
2019-02-05 16:11:19 +0900

    green for initial state

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
   render() {
     return (
       <div data-test="component-app">
