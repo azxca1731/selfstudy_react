c274efb12920e765471f56f37fd0bb2a2b078611
azxca1731 <azxca1731@gmail.com>
2019-02-07 01:14:08 +0900

    add test util name of find by test attr

---
+++ jotto/test/testUtils.js
@@ -0,0 +1,9 @@
+/**
+ * Returns node(s) with the given data-test attribut.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
